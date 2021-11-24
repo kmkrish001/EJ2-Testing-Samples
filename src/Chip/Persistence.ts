94e5406969ca9a8570a4c65e8815e82a26870770
Muthukrishnan Kandasamy <muthukrishnan.kandasamy@syncfusion.com>
2021-11-24 11:39:44 +0530

    samples updated.

---
+++ src/Chip/Persistence.ts
@@ -0,0 +1,92 @@
+import { ChipList } from '@syncfusion/ej2-buttons';
+/**
+ * Default Chip sample
+ */
+ let defaultData = [
+    {
+        "text": "Apple",
+        "cssClass": "e-primary"
+    },
+    {
+        "text": "Microsoft",
+        "cssClass": "e-info"
+    },
+    {
+        "text": "Google",
+        "cssClass": "e-success"
+    },
+    {
+        "text": "Tesla",
+        "cssClass": "e-warning"
+    },
+    {
+        "text": "Intel",
+        "cssClass": "e-danger"
+    }
+    ];
+
+    let avatarData = [{
+        "text": "Anne",
+        "leadingIconCss": "anne"
+    },
+    {
+        "text": "Janet",
+        "leadingIconCss": "janet"
+    },
+    {
+        "text": "Laura",
+        "leadingIconCss": "laura"
+    },
+    {
+        "text": "Margaret",
+        "leadingIconCss": "margaret"
+    }
+    ];
+    let filterData = [{
+        "text": "Extra small"
+    },
+    {
+        "text": "Small"
+    },
+    {
+        "text": "Medium"
+    },
+    {
+        "text": "Large"
+    },
+    {
+        "text": "Extra large"
+    }
+    ];
+    let choiceData = [{
+        "text": "Send a text",
+        "leadingIconCss": "text"
+    },
+    {
+        "text": "Set a reminder",
+        "leadingIconCss": "alarm"
+    },
+    {
+        "text": "Read my emails",
+        "leadingIconCss": "mail"
+    },
+    {
+        "text": "Play a game",
+        "leadingIconCss": "game"
+    }];
+    // initialize default chip
+    new ChipList({ chips: defaultData, enablePersistence: true }, '#chip-default');
+
+    // initialize avatar chip
+    new ChipList({ chips: avatarData, enableDelete: true , enablePersistence: true}, '#chip-avatar');
+
+    // initialize filter chip
+    new ChipList({ chips: filterData, selection: 'Multiple', selectedChips: [1, 3], enablePersistence: true }, '#chip-filter');
+
+    // initialize choice chip
+    new ChipList(
+        {
+            chips: choiceData, selection: 'Single', cssClass: 'e-outline',
+            selectedChips: [1], enablePersistence: true
+        },
+        '#chip-choice');
--- src/system.config.js
+++ src/system.config.js
@@ -16,16 +16,15 @@ System.config({
         "@syncfusion/ej2-lists": "syncfusion:ej2-lists/dist/ej2-lists.umd.min.js",
         "@syncfusion/ej2-navigations": "syncfusion:ej2-navigations/dist/ej2-navigations.umd.min.js",
         "@syncfusion/ej2-popups": "syncfusion:ej2-popups/dist/ej2-popups.umd.min.js",
-        "@syncfusion/ej2-charts": "syncfusion:ej2-charts/dist/ej2-charts.umd.min.js",
         "@syncfusion/ej2-calendars": "syncfusion:ej2-calendars/dist/ej2-calendars.umd.min.js",
-        "@syncfusion/ej2-compression": "syncfusion:ej2-compression/dist/ej2-compression.umd.min.js",
-        "@syncfusion/ej2-file-utils": "syncfusion:ej2-file-utils/dist/ej2-file-utils.umd.min.js",
+        "@syncfusion/ej2-grids": "syncfusion:ej2-grids/dist/ej2-grids.umd.min.js",
+        "@syncfusion/ej2-filemanager": "syncfusion:ej2-filemanager/dist/ej2-filemanager.umd.min.js",
+        "@syncfusion/ej2-dropdowns": "syncfusion:ej2-dropdowns/dist/ej2-dropdowns.umd.min.js",
         "@syncfusion/ej2-excel-export": "syncfusion:ej2-excel-export/dist/ej2-excel-export.umd.min.js",
         "@syncfusion/ej2-pdf-export": "syncfusion:ej2-pdf-export/dist/ej2-pdf-export.umd.min.js",
-        "@syncfusion/ej2-grids": "syncfusion:ej2-grids/dist/ej2-grids.umd.min.js",
-        "@syncfusion/ej2-dropdowns": "syncfusion:ej2-dropdowns/dist/ej2-dropdowns.umd.min.js",
-        "@syncfusion/ej2-querybuilder": "syncfusion:ej2-querybuilder/dist/ej2-querybuilder.umd.min.js",
-        "@syncfusion/ej2-spreadsheet": "syncfusion:ej2-spreadsheet/dist/ej2-spreadsheet.umd.min.js"
+        "@syncfusion/ej2-compression": "syncfusion:ej2-compression/dist/ej2-compression.umd.min.js",
+        "@syncfusion/ej2-file-utils": "syncfusion:ej2-file-utils/dist/ej2-file-utils.umd.min.js",
+        "@syncfusion/ej2-layouts": "syncfusion:ej2-layouts/dist/ej2-layouts.umd.min.js"
     },
     packages: {
         'app': { main: 'app', defaultExtension: 'js' }
